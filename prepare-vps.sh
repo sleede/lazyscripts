2e068ffb4ba0327b8fef5993ce638893409159aa
Sylvain <bond.never.die@gmail.com>
2017-12-14 09:38:33 +0100

    translate in english + allow usage by pipe bash

--- prepare-vps.sh
+++ prepare-vps.sh
@@ -1,62 +1,100 @@
 #!/bin/bash
 
 # Make sure only root can run our script
-if [ $(id -u) -ne 0 ]; then
+if [[ "$(id -u)" -ne 0 ]]
+ then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
-echo -n "Mettre à jour le systeme ? (o/n) "
-read touche
-if [ "$touche" = "o" ];then
-  apt-get update && sudo apt-get upgrade
-fi
-
-echo -n "Mettre à jour la timezone ? (o/n) "
-read touche
-if [ "$touche" = "o" ];then
-  apt-get install ntp ntpdate
-  dpkg-reconfigure tzdata
-fi
-
-echo -n "Définir / modifier le mdp root (utile pour le recovery mode) ? (o/n) "
-read touche
-if [ "$touche" = "o" ];then
-  passwd
-fi
-
-echo -n "Désactiver le login root par mdp ? (only rsa key) (o/n) "
-read touche
-if [ "$touche" = "o" ];then
-  sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
-  /etc/init.d/ssh restart
-fi
-
-echo -n "Ajouter la swap ? (fortement recommandé) (o/n) "
-read touche
-if [ "$touche" = "o" ];then
-  read -p "Saisir un entier de 1 à 4 : " swap_value
-  echo "creating ${swap_value}G swap file"
-  fallocate -l ${swap_value}G /swapfile
-  chmod 600 /swapfile
-  mkswap /swapfile
-  swapon /swapfile
-  echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
-  echo 'vm.swappiness = 10' >> /etc/sysctl.conf
-  echo 'vm.vfs_cache_pressure=50' >> /etc/sysctl.conf
-fi
-
-echo -n "Monter un volume /dev/vdx ? (o/n) "
-read touche
-if [ "$touche" = "o" ];then
-  fdisk -l
-  read -p "indiquer le nom du volume (ex vdb) : " volume_name
-  read -p "Etes vous sur de vouloir formater et monter le volume /dev/${volume_name} sur /apps ? (o/n) : " touche
-  if [ "$touche" = "o" ];then
-    mkfs -t ext4 /dev/${volume_name}
-    mkdir -p /apps
-    mount /dev/${volume_name} /apps
-    uuid_value=$(blkid -o value -s UUID /dev/${volume_name})
-    echo "UUID=\"${uuid_value}\" /apps ext4 defaults 0 2" >> /etc/fstab
+upgrade()
+{
+  echo -n "Upgrade the system? (y/n) "
+  read -r touche </dev/tty
+  if [[ "$touche" = "y" || "$touche" = "o" ]]
+   then
+    apt-get update && sudo apt-get upgrade
   fi
-fi
+}
+
+timezone()
+{
+  echo -n "Update timezone? (y/n) "
+  read -r touche </dev/tty
+  if [[ "$touche" = "y" || "$touche" = "o" ]]
+  then
+    apt-get install ntp ntpdate
+    dpkg-reconfigure tzdata
+  fi
+}
+
+password()
+{
+  echo -n "Define / change the root password (usefull for recovery mode)? (y/n) "
+  read -r touche </dev/tty
+  if [[ "$touche" = "y" || "$touche" = "o" ]]
+  then
+    passwd
+  fi
+}
+
+force_rsa()
+{
+  echo -n "Disable root login using password? (only rsa key) (y/n) "
+  read -r touche </dev/tty
+  if [[ "$touche" = "o" || "$touche" = "o" ]]
+  then
+    sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+    /etc/init.d/ssh restart
+  fi
+}
+
+add_swap()
+{
+  echo -n "Add swap? (highly recommended) (y/n) "
+  read -r touche </dev/tty
+  if [[ "$touche" = "y" || "$touche" = "o" ]]
+  then
+    read -rp "What's the size (in GB)? Please input a number between 1 and 4: " swap_value </dev/tty
+    echo "creating ${swap_value}G swap file"
+    fallocate -l "${swap_value}G" /swapfile
+    chmod 600 /swapfile
+    mkswap /swapfile
+    swapon /swapfile
+    echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+    echo 'vm.swappiness = 10' >> /etc/sysctl.conf
+    echo 'vm.vfs_cache_pressure=50' >> /etc/sysctl.conf
+  fi
+}
+
+mount_vol()
+{
+  echo -n "Mount a volume (like /dev/vdx)? (o/n) "
+  read -r touche </dev/tty
+  if [[ "$touche" = "y" || "$touche" = "o" ]]
+  then
+    fdisk -l
+    read -rp "Specify volume name (eg. vdb): " volume_name </dev/tty
+    read -rp "Do you really want to format and mount the volume /dev/${volume_name} on /apps? (y/n) : " touche </dev/tty
+    if [[ "$touche" = "y" || "$touche" = "o" ]]
+    then
+      mkfs -t ext4 "/dev/${volume_name}"
+      mkdir -p /apps
+      mount "/dev/${volume_name}" /apps
+      uuid_value=$(blkid -o value -s UUID /dev/${volume_name})
+      echo "UUID=\"${uuid_value}\" /apps ext4 defaults 0 2" >> /etc/fstab
+    fi
+  fi
+}
+
+vps_prepare()
+{
+  upgrade
+  timezone
+  password
+  force_rsa
+  add_swap
+  mount_vol
+}
+
+vps_prepare "$@"
